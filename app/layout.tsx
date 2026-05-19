12e264a89de6e109abfd50edfed56640d43e01dc
milosz94 <zivanovic.milos94@hotmail.com>
2026-05-19 19:36:04 +0200

    umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -13,7 +14,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-white text-gray-900">{children}</body>
+      <body className="bg-white text-gray-900">
+        {children}
+        <Script
+          src="https://umami-foyrsg53.redu.cloud/script.js"
+          data-website-id="cb152712-190c-4a26-905f-42aef233eb0a"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
